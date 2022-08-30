4ba41dd57469e63517b4df6daf4ea4433d8534d0
Arturo Alfaro Blanco <76973343+ArthurAlphaW@users.noreply.github.com>
2022-08-30 03:18:54 -0500

    Fixed Tech test

--- src/js/main.js
+++ src/js/main.js
@@ -5,19 +5,22 @@ var reportsWidget = {
             '{{#.}}' +
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
+                            '<div class="report_info">'+
                             '<h3 class="reports_title">' +
                                 '<a href="{{url}}" target="_blank">{{title}}</a>' +
                             '</h3>' +
+                            '<span>({{file_size}} {{file_type}}) </span>' +
+                            '</div>'+
                         '{{/documents}}' +
                     '</footer>' +
                 '</article>' +
             '{{/.}}'
         )
-    },
+    }, 
 
     init: function() {
         this.renderReports(reportData || []);
